aea59e17328967017122a47a7eb10e387a8c208a
Omar <omaar_5@hotmail.com>
2021-08-22 17:37:58 +0200

    complete: Feature part-1

--- components/Feature/Feature.js
+++ components/Feature/Feature.js
@@ -1,11 +1,49 @@
+import { Container, Section, Button } from "../../global";
 import {
-    FeatureContainer
+    FeatureContainer,
+    ImageContainer,
+    HomeData,
+    HomeHeader,
+    Title,
+    SubTitle,
+    HomeFooter,
+    FooterTitle,
+    Text,
+    ButtonSpan,
+    ButtonIcon,
+    HomePrice,
 } from "./styledFeature";
 
-export const Feature = () => {
+export const Feature = ({ data }) => {
     return (
-        <FeatureContainer>
-            Feature Component
-        </FeatureContainer>
+        <Section>
+            <Container>
+                <FeatureContainer>
+                    <ImageContainer>
+                        <img src="/img/home.png" alt="Home Image" />
+                    </ImageContainer>
+
+                    <HomeData>
+                        <HomeHeader>
+                            <Title>On ear</Title>
+                            <SubTitle>Beats 3</SubTitle>
+                        </HomeHeader>
+                        <HomeFooter>
+                            <FooterTitle>Overview</FooterTitle>
+                            <Text>{data.text}</Text>
+                            <Button flex>
+                                <ButtonIcon>
+                                    <i className="ri-shopping-bag-line"></i>
+                                </ButtonIcon>
+                                <ButtonSpan>
+                                    Add to Bag
+                                </ButtonSpan>
+                                <HomePrice>$299</HomePrice>
+                            </Button>
+                        </HomeFooter>
+                    </HomeData>
+                </FeatureContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Feature/styledFeature.js
+++ components/Feature/styledFeature.js
@@ -1,3 +1,64 @@
 import styled from 'styled-components';
 
-export const FeatureContainer = styled.div``;
+export const FeatureContainer = styled.div`
+  display: grid;
+`;
+
+export const ImageContainer = styled.div`
+  img {
+    width: 250px;
+    position: absolute;
+    top: -16rem;
+    right: 1.5rem;
+  }
+`;
+
+export const HomeData = styled.div`
+  padding-top: 5rem;
+`;
+
+export const HomeHeader = styled.div`
+  position: relative;
+`;
+
+export const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fonts.biggestFontSize};
+  background: ${({ theme }) => theme.colors.textGradient};
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  position: absolute;
+  top: -6rem;
+  left: 1rem;
+  line-height: 6rem;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: ${({ theme }) => theme.fonts.bigFontSize};
+  margin-bottom: 2.5rem;
+`;
+
+export const HomeFooter = styled.div``;
+
+export const FooterTitle = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.h3FontSize};
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.medium};
+  margin-bottom: 1rem;
+`;
+
+export const Text = styled.p`
+  margin-bottom: 2.5rem;
+  line-height: ${({ theme }) => theme.fonts.textLineHeight};
+`;
+
+export const HomePrice = styled.span`
+  font-size: ${({ theme }) => theme.fonts.h3FontSize};
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.semiBold};
+  margin-left: 0.75rem;
+`;
+
+export const ButtonIcon = styled.div`
+  font-size: 1.2rem;
+`;
+
+export const ButtonSpan = styled.span``;
