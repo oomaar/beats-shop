d0b12800b6b8566c1a4a7048245d0743b95c75e5
Omar <omaar_5@hotmail.com>
2021-08-22 20:22:41 +0200

    complete: Specs part-1

--- components/Specs/Specs.js
+++ components/Specs/Specs.js
@@ -1,11 +1,49 @@
+import { Container, Section, SectionTitle } from "../../global";
 import {
     SpecsContainer,
+    SpecsSubContainer,
+    SpecsContent,
+    SpecsData,
+    Title,
+    SubTitle,
+    Img,
+    Icon,
 } from "./styledSpecs";
 
-export const Specs = () => {
+export const Specs = ({ data }) => {
+    const specsData = data.map(spec => (
+        <SpecsData key={spec.id}>
+            <Icon>
+                <i className={spec.icon}></i>
+            </Icon>
+            <Title>{spec.title}</Title>
+            {spec.id === 4 ? (
+                <SubTitle>{spec.subTitle} <br /> {spec.text}</SubTitle>
+            ) : (
+                <SubTitle>{spec.subTitle}</SubTitle>
+            )}
+        </SpecsData>
+    ));
+
     return (
-        <SpecsContainer>
-            Specs Component
-        </SpecsContainer>
+        <Section>
+            <SpecsContainer>
+                <SectionTitle gradient>Specs</SectionTitle>
+
+                <div>
+                    <Container>
+                        <SpecsSubContainer>
+                            <SpecsContent>
+                                {specsData}
+                            </SpecsContent>
+
+                            <div>
+                                <Img src="/img/specs.png" alt="" />
+                            </div>
+                        </SpecsSubContainer>
+                    </Container>
+                </div>
+            </SpecsContainer>
+        </Section>
     );
 };
--- components/Specs/styledSpecs.js
+++ components/Specs/styledSpecs.js
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
 
-export const SpecsContainer = styled.div``;
+export const SpecsContainer = styled.div`
+  display: grid;
+`;
+
+export const SpecsSubContainer = styled.div`
+  display: grid;
+  position: relative;
+`;
+
+export const SpecsContent = styled.div`
+  display: grid;
+  row-gap: 1.5rem;
+`;
+
+export const SpecsData = styled.div`
+  display: grid;
+  row-gap: 0.25rem;
+
+  :nth-child(1),
+  :nth-child(4) {
+    margin-left: 1.5rem;
+  }
+`;
+
+export const Icon = styled.div`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.whiteColor};
+`;
+
+export const Title = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.normalFontSize};
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.medium};
+`;
+
+export const SubTitle = styled.span`
+  font-size: ${({ theme }) => theme.fonts.smallerFontSize};
+`;
+
+export const Img = styled.img`
+  width: 250px;
+  position: absolute;
+  top: 2rem;
+  right: -4rem;
+`;
