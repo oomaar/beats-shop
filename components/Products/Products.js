93c4de1e592b0f950c966351b0cb44adbb6dd7c3
Omar <omaar_5@hotmail.com>
2021-08-23 01:01:18 +0200

    complete: Products part-1

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,11 +1,35 @@
+import { Button, Container, Section, SectionTitle } from "../../global";
 import {
     ProductsContainer,
+    ProductsCard,
+    ProductsContent,
+    Img,
+    ProductsTitle,
+    ProductsPrice,
 } from "./styledProducts";
 
-export const Products = () => {
+export const Products = ({ data }) => {
+    const productsData = data.map(product => (
+        <ProductsCard key={product.id}>
+            <ProductsContent>
+                <Img src={product.img} alt={product.title} />
+                <ProductsTitle>{product.title}</ProductsTitle>
+                <ProductsPrice>${product.price}</ProductsPrice>
+                <Button flex products>
+                    <i className="ri-shopping-bag-line"></i>
+                </Button>
+            </ProductsContent>
+        </ProductsCard>
+    ));
     return (
-        <ProductsContainer>
-            Products component
-        </ProductsContainer>
+        <Section>
+            <SectionTitle gradient>Choose <br /> Your Style</SectionTitle>
+
+            <Container>
+                <ProductsContainer>
+                    {productsData}
+                </ProductsContainer>
+            </Container>
+        </Section>
     )
 }
--- components/Products/styledProducts.js
+++ components/Products/styledProducts.js
@@ -1,3 +1,38 @@
 import styled from 'styled-components';
 
-export const ProductsContainer = styled.div``;
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5rem 1.5rem;
+  padding-top: 3rem;
+`;
+
+export const ProductsCard = styled.article`
+  position: relative;
+  height: 132px;
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  display: grid;
+`;
+
+export const ProductsContent = styled.div`
+  align-self: flex-end;
+`;
+
+export const Img = styled.img`
+  width: 80px;
+  position: absolute;
+  inset: 0;
+  margin: 0 auto;
+  top: -3rem;
+`;
+
+export const ProductsTitle = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.smallFontSize};
+`;
+
+export const ProductsPrice = styled.span`
+  font-size: ${({ theme }) => theme.fonts.smallFontSize};
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.semiBold};
+`;
