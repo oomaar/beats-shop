afe01da9b9b8b035d1fb2894639b1173124efedd
Omar <omaar_5@hotmail.com>
2021-08-23 01:44:52 +0200

    complete: Footer part-1

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,80 @@
+import { Button, Container, Section } from "../../global";
 import {
     FooterContainer,
+    Logo,
+    FooterContent,
+    FooterTitle,
+    FooterLinks,
+    FooterLink,
+    Form,
+    Input,
+    FooterSocial,
+    SocialLink,
+    FooterCopy,
+    HSix,
 } from "./styledFooter";
 
-export const Footer = () => {
+export const Footer = ({ data }) => {
+    const productsLinks = data.productsLinks.map(link => (
+        <li key={link.id}>
+            <FooterLink href="#">{link.title}</FooterLink>
+        </li>
+    ));
+
+    const supportLinks = data.supportLinks.map(link => (
+        <li key={link.id}>
+            <FooterLink href="#">{link.title}</FooterLink>
+        </li>
+    ));
+
+    const socialLinks = data.socialLinks.map(link => (
+        <SocialLink key={link.id}>
+            <i className={link.icon}></i>
+        </SocialLink>
+    ));
     return (
-        <FooterContainer>
-            Footer Component
-        </FooterContainer>
+        <Section>
+            <Container>
+                <FooterContainer>
+                    <Logo href="#">
+                        <img src="/img/logo.png" alt="Logo" />
+                    </Logo>
+
+                    <FooterContent>
+                        <FooterTitle>Products</FooterTitle>
+
+                        <FooterLinks>
+                            {productsLinks}
+                        </FooterLinks>
+                    </FooterContent>
+
+                    <FooterContent>
+                        <FooterTitle>Support</FooterTitle>
+
+                        <FooterLinks>
+                            {supportLinks}
+                        </FooterLinks>
+                    </FooterContent>
+
+                    <FooterContent>
+                        <Form action="">
+                            <Input type="email" placeholder="Email Address" />
+                            <Button flex>
+                                <i className="ri-send-plane-line"></i>
+                                Subscribe
+                            </Button>
+                        </Form>
+
+                        <FooterSocial>
+                            {socialLinks}
+                        </FooterSocial>
+                    </FooterContent>
+
+                    <FooterCopy>
+                        <HSix>&#169; Beats. All Rights Reserved</HSix>
+                    </FooterCopy>
+                </FooterContainer>
+            </Container>
+        </Section>
     )
 }
--- components/Footer/styledFooter.js
+++ components/Footer/styledFooter.js
@@ -1,3 +1,83 @@
 import styled from 'styled-components';
 
-export const FooterContainer = styled.footer``;
+export const FooterContainer = styled.footer`
+  display: grid;
+  row-gap: 2rem;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+export const Logo = styled.a`
+  width: 2rem;
+`;
+
+export const FooterContent = styled.div``;
+
+export const FooterTitle = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.h3FontSize};
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.medium};
+  margin-bottom: 1rem;
+`;
+
+export const FooterLinks = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+`;
+
+export const FooterLink = styled.a`
+  color: ${({ theme }) => theme.colors.textColor};
+
+  :hover {
+    color: ${({ theme }) => theme.colors.whiteColor};
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  column-gap: 0.5rem;
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  padding: 0.5rem 0.75rem;
+  border-radius: 0.5rem;
+  margin-bottom: 2rem;
+`;
+
+export const Input = styled.input`
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  width: 90%;
+  color: ${({ theme }) => theme.colors.whiteColor};
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.textColor};
+    font-size: ${({ theme }) => theme.fonts.normalFontSize};
+    font-family: ${({ theme }) => theme.fonts.bodyFont};
+  }
+`;
+
+export const FooterSocial = styled.div`
+  display: flex;
+  column-gap: 1.25rem;
+`;
+
+export const SocialLink = styled.a`
+  display: inline-flex;
+  color: ${({ theme }) => theme.colors.whiteColor};
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  transition: 0.4s;
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.blackColor};
+  }
+`;
+
+export const FooterCopy = styled.div`
+  margin-top: 5rem;
+  text-align: center;
+`;
+
+export const HSix = styled.h6`
+  font-size: ${({ theme }) => theme.fonts.smallerFontSize};
+  color: ${({ theme }) => theme.colors.textColorLight};
+`;
