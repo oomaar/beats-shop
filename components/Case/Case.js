241c3f2e3eb28bfd37fe8b2de74292c144cad3a7
Omar <omaar_5@hotmail.com>
2021-08-22 23:11:23 +0200

    complete: Case part-1

--- components/Case/Case.js
+++ components/Case/Case.js
@@ -1,11 +1,32 @@
+import { Button, Container, Section, SectionTitle } from "../../global";
 import {
     CaseContainer,
+    Img,
+    CaseData,
+    Text,
 } from "./styledCase";
 
-export const Case = () => {
+export const Case = ({ data }) => {
     return (
-        <CaseContainer>
-            Case Component
-        </CaseContainer>
+        <Section>
+            <SectionTitle gradient>Case</SectionTitle>
+
+            <Container>
+                <CaseContainer>
+                    <div>
+                        <Img src="/img/case.png" alt="case" />
+                    </div>
+
+                    <CaseData>
+                        <Text>{data.text}</Text>
+
+                        <Button flex>
+                            <i className="ri-information-line"></i>
+                            More info
+                        </Button>
+                    </CaseData>
+                </CaseContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Case/styledCase.js
+++ components/Case/styledCase.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const CaseContainer = styled.div``;
+export const CaseContainer = styled.div`
+  display: grid;
+  position: relative;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+export const Img = styled.img`
+  width: 250px;
+  position: absolute;
+  left: -7rem;
+`;
+
+export const CaseData = styled.div``;
+
+export const Text = styled.p`
+  margin-bottom: 1.5rem;
+  line-height: ${({ theme }) => theme.fonts.textLineHeight};
+`;
