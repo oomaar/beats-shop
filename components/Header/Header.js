3cf0f424f367bc625f7d6b6310a0022a10680204
Omar <omaar_5@hotmail.com>
2021-08-22 17:05:12 +0200

    complete: Header part-1

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,50 @@
+import { useState } from "react";
+import { Container } from "../../global";
 import {
-    MainHeader
+    MainHeader,
+    Nav,
+    Logo,
+    NavMenu,
+    NavList,
+    NavItem,
+    NavLink,
+    NavClose,
+    NavToggle,
 } from "./styledHeader";
 
-export const Header = () => {
+export const Header = ({ data }) => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const linksList = data.map(link => (
+        <NavItem key={link.id}>
+            <NavLink onClick={() => setShowMenu(false)} href={link.href}>{link.title}</NavLink>
+        </NavItem>
+    ));
+
     return (
         <MainHeader>
-            Header Component
+            <Container>
+                <Nav>
+                    <Logo href="#">
+                        <img
+                            src="/img/logo.png"
+                            alt="Beats Logo"
+                        />
+                    </Logo>
+
+                    <NavMenu showMenu={showMenu}>
+                        <NavList>
+                            {linksList}
+                        </NavList>
+                        <NavClose onClick={() => setShowMenu(false)}>
+                            <i className="ri-close-line"></i>
+                        </NavClose>
+                    </NavMenu>
+                    <NavToggle onClick={() => setShowMenu(true)}>
+                        <i className="ri-function-line"></i>
+                    </NavToggle>
+                </Nav>
+            </Container>
         </MainHeader>
     )
 }
--- components/Header/styledHeader.js
+++ components/Header/styledHeader.js
@@ -1,3 +1,77 @@
 import styled from 'styled-components';
+import { headerHeight } from '../../global';
 
-export const MainHeader = styled.header``;
+export const MainHeader = styled.header`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  background: transparent;
+`;
+
+export const Nav = styled.nav`
+  height: ${headerHeight};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.a`
+  display: flex;
+  width: 1.5rem;
+`;
+
+export const NavMenu = styled.div`
+  @media screen and (max-width: 767px) {
+    position: fixed;
+    background-color: ${({ theme }) => theme.colors.bodyColor};
+    top: ${({ showMenu }) => showMenu ? "0" : "-100%"};
+    left: 0;
+    width: 100%;
+    padding: 4rem 0 3rem;
+    transition: 0.4s;
+  }
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 1rem;
+`;
+
+export const NavItem = styled.li``;
+
+export const NavLink = styled.a`
+  font-size: ${({ theme }) => theme.fonts.h2FontSize};
+  text-transform: uppercase;
+  font-weight: ${({ theme }) => theme.fonts.fontWeight.semiBold};
+  background: ${({ theme }) => theme.colors.textGradient};
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  transition: 0.4s;
+
+  :hover {
+    background: ${({ theme }) => theme.colors.whiteColor};
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+`;
+
+export const NavClose = styled.div`
+  position: absolute;
+  font-size: 1.5rem;
+  top: 1rem;
+  right: 1rem;
+  color: ${({ theme }) => theme.colors.whiteColor};
+  cursor: pointer;
+`;
+
+export const NavToggle = styled.div`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.whiteColor};
+  cursor: pointer;
+`;
