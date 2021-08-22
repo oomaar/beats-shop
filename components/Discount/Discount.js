57e9a88abb03bebb7203b14932dd64fa6306c432
Omar <omaar_5@hotmail.com>
2021-08-23 00:34:45 +0200

    complete: Discount part-1

--- components/Discount/Discount.js
+++ components/Discount/Discount.js
@@ -1,11 +1,29 @@
+import { Button, Container, Section } from "../../global";
 import {
     DiscountContainer,
+    DiscountAnimate,
+    DiscountTitle,
+    DiscountText,
 } from "./styledDiscount";
 
 export const Discount = () => {
     return (
-        <DiscountContainer>
-            Discount Component
-        </DiscountContainer>
+        <Section>
+            <Container>
+                <DiscountContainer>
+                    <DiscountAnimate>
+                        <DiscountTitle>Immerse yourself in <br /> your music</DiscountTitle>
+                        <DiscountText>Get it now, up to 50% off.</DiscountText>
+
+                        <Button flex>
+                            <i className="ri-shopping-bag-line"></i>
+                            Shop Now
+                        </Button>
+                    </DiscountAnimate>
+
+                    <img src="/img/discount.png" alt="Discount" />
+                </DiscountContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Discount/styledDiscount.js
+++ components/Discount/styledDiscount.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const DiscountContainer = styled.div``;
+export const DiscountContainer = styled.div`
+  display: grid;
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.containerColor};
+  padding: 2rem 1.5rem;
+  border-radius: 0.75rem;
+  margin-top: 6rem;
+
+  img {
+    width: 300px;
+    position: absolute;
+    top: 4rem;
+    right: -11rem;
+  }
+`;
+
+export const DiscountAnimate = styled.div``;
+
+export const DiscountTitle = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.h3FontSize};
+  margin-bottom: 0.75rem;
+`;
+
+export const DiscountText = styled.p`
+  margin-bottom: 1rem;
+`;
