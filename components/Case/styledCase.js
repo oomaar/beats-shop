dbfb7c8c18522fb39567626ad909dcddf5f30302
Omar <omaar_5@hotmail.com>
2021-08-23 19:20:29 +0200

    complete: Case part-2 (small-screen)

--- components/Case/styledCase.js
+++ components/Case/styledCase.js
@@ -1,15 +1,26 @@
 import styled from 'styled-components';
+import { smallScreen } from '../../global/styles/GlobalStyle';
 
 export const CaseContainer = styled.div`
   display: grid;
   position: relative;
   grid-template-columns: repeat(2, 1fr);
+
+  @media screen and (max-width: ${smallScreen}) {
+    grid-template-columns: 0.6fr 1fr;
+  }
 `;
 
 export const Img = styled.img`
   width: 250px;
   position: absolute;
   left: -7rem;
+
+  @media screen and (max-width: ${smallScreen}) {
+    width: 220px;
+    top: -2rem;
+    left: -9rem;
+  }
 `;
 
 export const CaseData = styled.div``;
